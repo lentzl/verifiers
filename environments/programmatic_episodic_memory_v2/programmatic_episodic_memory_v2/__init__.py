1afdeeee99b5c8662c435975d4d28d1904265fa9
lentzl <41491017+lentzl@users.noreply.github.com>
2026-08-14 07:42:49 +0200

    Export memory feedback contract

--- environments/programmatic_episodic_memory_v2/programmatic_episodic_memory_v2/__init__.py
+++ environments/programmatic_episodic_memory_v2/programmatic_episodic_memory_v2/__init__.py
@@ -1,3 +1,13 @@
+from programmatic_episodic_memory_v2.feedback import (
+    FEEDBACK_SCHEMA_VERSION,
+    MemoryFailureCategory,
+    MemoryFailureCode,
+    MemoryFailureDiagnostic,
+    MemoryFailureSignals,
+    diagnose_memory_failure,
+    feedback_contract_payload,
+    render_memory_feedback,
+)
 from programmatic_episodic_memory_v2.taskset import (
     ProgrammaticEpisodicMemoryConfig,
     ProgrammaticEpisodicMemoryEnv,
@@ -5,7 +15,15 @@ from programmatic_episodic_memory_v2.taskset import (
 )
 
 __all__ = [
+    "FEEDBACK_SCHEMA_VERSION",
+    "MemoryFailureCategory",
+    "MemoryFailureCode",
+    "MemoryFailureDiagnostic",
+    "MemoryFailureSignals",
     "ProgrammaticEpisodicMemoryConfig",
     "ProgrammaticEpisodicMemoryEnv",
     "ProgrammaticEpisodicMemoryTaskset",
+    "diagnose_memory_failure",
+    "feedback_contract_payload",
+    "render_memory_feedback",
 ]
